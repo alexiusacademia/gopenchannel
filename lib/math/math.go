73d021d972ba850e3ad4547951a9cdb9242f1db1
Alexius Academia <alexius.academia@gmail.com>
2022-01-23 19:28:58 +0800

    Interpolate function

---
+++ lib/math/math.go
@@ -0,0 +1,8 @@
+package math
+
+// Function for linear interpolation
+func Interpolate(x1 float64, x3 float64, y1 float64, y2 float64, y3 float64) (x2 float64) {
+	x2 = (x1-x3)*(y2-y3)/(y1-y3) + x3
+
+	return
+}
